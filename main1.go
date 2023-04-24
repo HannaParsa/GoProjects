81ffa409db76bddb72dd9fdcd07002d12747ef75
hanna-parsa-80 <hanaparsa_80@yahoo.com>
2023-04-24 23:18:08 +0330

    in progress

--- main1.go
+++ main1.go
@@ -1,9 +1,10 @@
+//this is my project for learning go
 package main
 import("fmt")
 
 func main(){
 	string student1 := "student1"
 	var student2 string = "student2"
-	fmt.Println("selcom!")
+	fmt.Println("Welcom!")
 
 }

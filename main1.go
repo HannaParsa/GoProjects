d26bf477b483b99ade0d0c73ecc369247a501229
hanna-parsa-80 <hanaparsa_80@yahoo.com>
2023-04-25 22:47:15 +0330

    -

--- main1.go
+++ main1.go
@@ -4,8 +4,9 @@ import("fmt")
 
 const PI float32 = 3.14
 func main(){
-	string student1 := "student1"
-	var student2 string = "student2"
+	string student1 := "student1" //string
+	var student2 string = "student2" //stirng
+	var count int = 2 //int
 	fmt.Println("Welcom!")
 
 }

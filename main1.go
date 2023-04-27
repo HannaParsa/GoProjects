5894d99f69762d91034c1a3d3315e12a00c9e237
hanna-parsa-80 <hanaparsa_80@yahoo.com>
2023-04-27 12:30:44 +0330

    learning

--- main1.go
+++ main1.go
@@ -7,6 +7,8 @@ func main(){
 	string student1 := "student1" //string
 	var student2 string = "student2" //stirng
 	var count int = 2 //int
+    courses := [2] string {"math", "physics"} // arr = [length]type{values}
+	var grades =[...] string {"pass","fail"} 
 	fmt.Println("Welcom!")
 	ftm.Println("student name is: " student1)
 	ftm.Println("student name is: " student2)
